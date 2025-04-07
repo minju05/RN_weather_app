a506e7381c370897ff0f81a07bf8ec7f74a22fd8
minju05 <water0501@naver.com>
2025-04-07 09:06:08 +0900

    8_ui_number

--- App.js
+++ App.js
@@ -1,12 +1,65 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
+// 8강 PPT [App.js: 기본 UI] //
+/*
 export default function App() {
-  return (
+return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+  );
+}
+*/
+
+// 8강 PPT [숫자 구현] //
+/*
+export default function App() {
+  const [number, setNumber] = useState(0);
+
+  return (
+      <View style={styles.container}>
+        <Text style={{ fontSize: 50 }}>결과: {number} </Text>
+        <StatusBar style="auto" />
+      </View>
+  );
+}
+*/
+
+// 8강 PPT [버튼 구현] //
+/*
+export default function App() {
+  const [number, setNumber] = useState(0);
+
+  return (
+      <View style={styles.container}>
+        <Text style={{ fontSize: 50 }}>결과: {number} </Text>
+        <View style={{ flexDirection: 'row', gap: 50 }}>
+          <Button title="증가" />
+          <Button title="감소" />
+        </View>
+        <StatusBar style="auto" />
+      </View>
+  );
+}
+*/
+
+// 8강 PPT [숫자 증감 구현] //
+
+export default function App() {
+  const [number, setNumber] = useState(0);
+
+  return (
+      <View style={styles.container}>
+        <Text style={{ fontSize: 50 }}>결과: {number} </Text>
+        <View style={{ flexDirection: 'row', gap: 50 }}>
+          <Button title="증가" onPress={() => setNumber(number + 1)} />
+          <Button title="감소" onPress={() => setNumber(number - 1)} />
+        </View>
+        <StatusBar style="auto" />
+      </View>
   );
 }
 
