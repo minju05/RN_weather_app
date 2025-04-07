45a6981087b799e9dd28a160505d9e82c4f78ecd
minju05 <water0501@naver.com>
2025-04-07 16:36:06 +0900

    9_RN_rules

--- App.js
+++ App.js
@@ -2,67 +2,17 @@ import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
-// 8강 PPT [App.js: 기본 UI] //
-/*
+// 9강 PPT [React Native: No tags] //
+
 export default function App() {
-return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-*/
-
-// 8강 PPT [숫자 구현] //
-/*
-export default function App() {
-  const [number, setNumber] = useState(0);
-
-  return (
-      <View style={styles.container}>
-        <Text style={{ fontSize: 50 }}>결과: {number} </Text>
-        <StatusBar style="auto" />
-      </View>
-  );
-}
-*/
-
-// 8강 PPT [버튼 구현] //
-/*
-export default function App() {
-  const [number, setNumber] = useState(0);
-
-  return (
-      <View style={styles.container}>
-        <Text style={{ fontSize: 50 }}>결과: {number} </Text>
-        <View style={{ flexDirection: 'row', gap: 50 }}>
-          <Button title="증가" />
-          <Button title="감소" />
+    return (
+        <View style={styles.container}>
+            {<Text style={{ fontSize: 50 }}>리액트 네이티브</Text>}
+            <span>리액트 네이티브</span>
+            <StatusBar style="auto" />
         </View>
-        <StatusBar style="auto" />
-      </View>
-  );
+    );
 }
-*/
-
-// 8강 PPT [숫자 증감 구현] //
-
-export default function App() {
-  const [number, setNumber] = useState(0);
-
-  return (
-      <View style={styles.container}>
-        <Text style={{ fontSize: 50 }}>결과: {number} </Text>
-        <View style={{ flexDirection: 'row', gap: 50 }}>
-          <Button title="증가" onPress={() => setNumber(number + 1)} />
-          <Button title="감소" onPress={() => setNumber(number - 1)} />
-        </View>
-        <StatusBar style="auto" />
-      </View>
-  );
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -71,3 +21,103 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+// 9강 PPT [React Native: No Shorthand Style] //
+/*
+export default function App() {
+  return (
+      <View style={styles.container}>
+        <Text style={{ fontSize: 50 }}>리액트 네이티브</Text>
+        <StatusBar style="auto" />
+      </View>
+  );
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        // border: "1px solid red" //
+        borderWidth: 10,
+        borderColor: "red",
+    },
+});
+*/
+
+// 9강 PPT [React Native: No Inherit Style] //
+/*
+export default function App() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>리액트 네이티브</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+    container: {
+        color: 'red',
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {},
+});
+*/
+
+// 9강 PPT [React Native: Spread Syntax] //
+/*
+export default function App() {
+    return (
+        <View style={ styles.container }>
+            <Text style={{ ...styles.container, color: 'blue' }}>리액트 네이티브</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+    container: {
+        fontSize: 50,
+        color: 'red',
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
+*/
+
+// 9강 PPT [React Native: No StyleSheet ver.] //
+/*
+export default function App() {
+    return (
+        <View style={{
+            flex: 1,
+            backgroundColor: '#fff',
+            alignItems: 'center',
+            justifyContent: 'center',
+        }}>
+            <Text style={{
+                fontSize: 50,
+                fontWeight: 'bold'}}>리액트 네이티브</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
+}
+*/
+
+// 9강 PPT [참고. StatusBar 유무 차이] //
+/*
+export default function App() {
+    return (
+        <View style={styles.container}>
+            <Text style={{ fontSize: 50 }}>리액트 네이티브</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
+}
+*/
+
